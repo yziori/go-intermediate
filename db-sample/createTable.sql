336035e4ecf4aa803df6fbc0e97eb653fd79d3ca
yziori <ms6rx7861@gmail.com>
2025-01-08 02:18:26 +0900

    🔧 chore: サンプルデータベースの環境構築

---
+++ db-sample/createTable.sql
@@ -0,0 +1,18 @@
+-- 記事データを格納するためのテーブル
+CREATE TABLE IF NOT EXISTS articles (
+    article_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    contents TEXT NOT NULL,
+    username VARCHAR(100) NOT NULL,
+    nice INTEGER NOT NULL,
+    created_at DATETIME
+);
+
+-- コメントデータを格納するためのテーブル
+CREATE TABLE IF NOT EXISTS `comment` (
+    comment_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    article_id INTEGER UNSIGNED NOT NULL,
+    message TEXT NOT NULL,
+    created_at DATETIME,
+    FOREIGN KEY (article_id) REFERENCES articles(article_id)
+);

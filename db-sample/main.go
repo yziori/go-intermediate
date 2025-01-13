cf7e0e479ddb17c09d717ff4658dba6d5b231e08
yziori <ms6rx7861@gmail.com>
2025-01-13 12:55:02 +0900

    refactor: QueryメソッドからQueryRowに置き換え

--- db-sample/main.go
+++ db-sample/main.go
@@ -26,36 +26,32 @@ func main() {
 	// プログラムが終了するときコネクションがcloseされるようにする
 	defer db.Close()
 
-	articleID := 2
+	articleID := 1
 	const sqlStr = `
 		select *
 		from articles
 		where article_id = ?;
 	`
-	rows, err := db.Query(sqlStr, articleID)
+
+	row := db.QueryRow(sqlStr, articleID)
+	if err := row.Err(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var article models.Article
+	var createdTime sql.NullTime
+
+	err = row.Scan(&article.ID, &article.Title,
+		&article.Contents, &article.UserName, &article.NickNum, &createdTime)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer rows.Close()
 
-	articleArray := make([]models.Article, 0)
-	for rows.Next() {
-		var article models.Article
-		var createdTime sql.NullTime
-		err := rows.Scan(&article.ID, &article.Title, &article.Contents,
-			&article.UserName, &article.NickNum, &createdTime)
-
-		if createdTime.Valid {
-			article.CreatedAt = createdTime.Time
-		}
-
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			articleArray = append(articleArray, article)
-		}
+	if createdTime.Valid {
+		article.CreatedAt = createdTime.Time
 	}
 
-	fmt.Printf("%+v\n", articleArray)
+	fmt.Printf("%+v\n", article)
 }

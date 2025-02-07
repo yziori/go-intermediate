cae6d98801aae4ea181b8a7bae7becc887210ce1
takaaki tadano <takaaki.tadano@optim.co.jp>
2025-02-07 18:28:37 +0900

    🐛 fix: コメントテーブル名を修正
    - フォーマットした

--- db-sample/insertData.sql
+++ db-sample/insertData.sql
@@ -1,14 +1,16 @@
 -- 記事データ2つ
-insert into articles (title, contents, username, nice, created_at) values
-    ('firstPost', 'This is my first blog', 'saki', 2, now());
-
-insert into articles (title, contents, username, nice) values 
-    ('2nd', 'Second blog post', 'saki', 4);
-
-
+insert into articles (title, contents, username, nice, created_at)
+values (
+        'firstPost',
+        'This is my first blog',
+        'saki',
+        2,
+        now()
+    );
+insert into articles (title, contents, username, nice)
+values ('2nd', 'Second blog post', 'saki', 4);
 -- コメントデータ2つ
-insert into comment (article_id, message, created_at) values
-    (1, '1st comment yeah', now());
-
-insert into comment (article_id, message) values
-    (1, 'welcome');
+insert into comments (article_id, message, created_at)
+values (1, '1st comment yeah', now());
+insert into comments (article_id, message)
+values (1, 'welcome');
